a848edcce50ec95c00b2ea2f42b6de74f1a4609e
Anatol-19 <anatol@staffonly39.ru>
2023-12-05 23:13:18 +0200

    Сделал домашку 1

---
+++ lesson_1/lesson_1_task_6.py
@@ -0,0 +1,4 @@
+def print_greeting():
+    print("Привет, мир!")
+    print("P.S. Никто не любит 5е задание")
+print_greeting()
---
+++ lesson_1/lesson_1_task_8.py
@@ -0,0 +1,17 @@
+def print_number(num):
+    print(num)
+
+digit1 = input("Введите первую цифру номера телефона: ")
+digit2 = input("Введите вторую цифру номера телефона: ")
+digit3 = input("Введите третью цифру номера телефона: ")
+digit4 = input("Введите четвертую цифру номера телефона: ")
+digit5 = input("Введите пятую цифру номера телефона: ")
+digit6 = input("Введите шестую цифру номера телефона: ")
+digit7 = input("Введите седьмую цифру номера телефона: ")
+digit8 = input("Введите восьмую цифру номера телефона: ")
+digit9 = input("Введите девятую цифру номера телефона: ")
+digit10 = input("Введите десятую цифру номера телефона: ")
+digit11 = input("Введите одиннадцатую цифру номера телефона: ")
+
+phone_number = digit1 + digit2 + digit3 + digit4 + digit5 + digit6 + digit7 + digit8 + digit9 + digit10 + digit11
+print_number(phone_number)
