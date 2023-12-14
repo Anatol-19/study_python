20137f31fcf49781739fbbccf18b83014c51f28f
Anatol-19 <anatol@staffonly39.ru>
2023-12-14 22:42:29 +0200

    шаблон теста урок 4 QA

--- QA_SkyPro/lesson_4/test_string_utils.py
+++ QA_SkyPro/lesson_4/test_string_utils.py
@@ -1,85 +1,51 @@
 from string_utils import StringUtils
+util = StringUtils()
 
+xs = "skypro"
+x_cap = "Skypro"
+x_trim = "skypro"
+x3 = ("1:2:3")
+x_list = ["1", "2", "3"]
+xS = "SkyPro"
+x_t  = True
+x_delete = "Sky"
+x_f = False
+x9 = [1,2,3,4]
+x_str = "1, 2, 3, 4"
 
+def test_capitilize():
+    rest = util.capitilize(xs)
+    assert rest == x_cap
 
-####### Список проверяемых опций с описанием их JTBD: #######
-#### 1. capitilize(self, string: str) -> str:
-#         """
-#         Принимает на вход текст, делает первую букву заглавной и возвращает этот же текст
-#         Пример: `capitilize("skypro") -> "Skypro"`
-#         """
-#     
-#### 2. trim(self, string: str) -> str:
-#         """
-#         Принимает на вход текст и удаляет пробелы в начале, если они есть
-#         Пример: `trim("   skypro") -> "skypro"`
-#         """
-#     
-#### 3. to_list(self, string: str, delimeter = ",") -> list[str]:
-#         """
-#         Принимает на вход текст с разделителем и возвращает список строк. \n
-#         Параметры: \n 
-#             `string` - строка для обработки \n
-#             `delimeter` - разделитель строк. По умолчанию запятая (",") \n
-#         Пример 1: `to_list("a,b,c,d") -> ["a", "b", "c", "d"]`
-#         Пример 2: `to_list("1:2:3", ":") -> ["1", "2", "3"]`
-#         """
-#     
-#### 4. contains(self, string: str, symbol: str) -> bool:
-#         """
-#         Возвращает `True`, если строка содержит искомый символ и `False` - если нет \n 
-#         Параметры: \n 
-#             `string` - строка для обработки \n
-#             `symbol` - искомый символ \n
-#         Пример 1: `contains("SkyPro", "S") -> True`
-#         Пример 2: `contains("SkyPro", "U") -> False`
-#         """
-#     
-#### 5. delete_symbol(self, string: str, symbol: str) -> str:
-#         """
-#         Удаляет все подстроки из переданной строки \n 
-#         Параметры: \n 
-#             `string` - строка для обработки \n
-#             `symbol` - искомый символ для удаления \n
-#         Пример 1: `delete_symbol("SkyPro", "k") -> "SyPro"`
-#         Пример 2: `delete_symbol("SkyPro", "Pro") -> "Sky"`
-#         """
-#     
-#### 6. def starts_with(self, string: str, symbol: str) -> bool:
-#         """
-#         Возвращает `True`, если строка начинается с заданного символа и `False` - если нет \n 
-#         Параметры: \n 
-#             `string` - строка для обработки \n
-#             `symbol` - искомый символ \n
-#         Пример 1: `starts_with("SkyPro", "S") -> True`
-#         Пример 2: `starts_with("SkyPro", "P") -> False`
-#         """
-#
-#### 7. end_with(self, string: str, symbol: str) -> bool:
-#         """
-#         Возвращает `True`, если строка заканчивается заданным символом и `False` - если нет \n 
-#         Параметры: \n 
-#             `string` - строка для обработки \n
-#             `symbol` - искомый символ \n
-#         Пример 1: `end_with("SkyPro", "o") -> True`
-#         Пример 2: `end_with("SkyPro", "y") -> False`
-#         """
-#   
-#### 8. is_empty(self, string: str) -> bool:
-#         """
-#         Возвращает `True`, если строка пустая и `False` - если нет \n 
-#         Пример 1: `is_empty("") -> True`
-#         Пример 2: `is_empty(" ") -> True`
-#         Пример 3: `is_empty("SkyPro") -> False`
-#         """
-#     
-#### 9. list_to_string(self, lst: list, joiner=", ") -> str:
-#         """
-#         Преобразует список элементов в строку с указанным разделителем \n 
-#         Параметры: \n 
-#             `lst` - список элементов \n
-#             `joiner` - разделитель элементов в строке. По умолчанию запятая (", ") \n
-#         Пример 1: `list_to_string([1,2,3,4]) -> "1, 2, 3, 4"`
-#         Пример 2: `list_to_string(["Sky", "Pro"]) -> "Sky, Pro"`
-#         Пример 3: `list_to_string(["Sky", "Pro"], "-") -> "Sky-Pro"`
-#         """
+def test_trim():
+    rest = util.trim(xs)
+    assert rest == x_trim
+
+def test_list():
+    rest = util.to_list(x3, ":")
+    assert rest == x_list   
+
+def test_contains():
+    rest = util.contains(xS, "S")
+    assert rest == x_t  
+
+def test_delete():
+    rest = util.delete_symbol(xS, "Pro")
+    assert rest == x_delete
+   
+def test_start():
+    rest = util.starts_with(xS, "P")
+    assert rest == x_f
+
+def test_end():
+    rest = util.end_with(xS, "o")
+    assert rest == x_t
+
+def test_empty():
+    rest = util.is_empty(xS)
+    assert rest == x_f
+
+def test_string():
+    rest = util.list_to_string(x9)
+    assert rest == x_str
+  
