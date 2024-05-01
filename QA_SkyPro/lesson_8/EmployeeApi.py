66b28a16b82e1decdf5f13163a04881822aa8744
Anatol-19 <anatol@staffonly39.ru>
2024-05-01 09:42:28 +0200

    lesson_8 pat 1. Done 81%

---
+++ QA_SkyPro/lesson_8/EmployeeApi.py
@@ -0,0 +1,67 @@
+import requests
+from faker import Faker
+
+fake = Faker()
+
+class EmployeeApi:
+    def __init__(self, url):
+        self.url = url
+
+    ## Авторизация
+    def x_clients_auth(self, user = "roxy", password = "animal-fairy"):
+        credentional = {
+            "username": user,
+            "password": password
+        }
+        response = requests.post(f'{self.url}auth/login', json=credentional)
+        head = {
+            "x-client-token" : response.json()["userToken"]
+        }
+        return head
+
+    ## Список активных компаний
+    def get_list_company(self, params_to_add = None):
+        response = requests.get(f'{self.url}company', params = params_to_add)
+        company_list = response.json()
+        return company_list
+
+    ## Список сотрудников компании
+    def employee_list_active(self, Nomber_of_Company = 0):
+        id_comp = str(self.get_list_company({'active' : 'true'})[Nomber_of_Company]['id'])
+        response = requests.get(f'{self.url}employee', params = {"company" : id_comp} )
+        return response
+    
+    ## Новый Сотрудник
+    def add_new_employee(self, Nomber_of_Company = 0):
+        head = self.x_clients_auth()
+        employee_new = {
+            "id": 0,
+            "firstName": fake.unique.first_name(),
+            "lastName": fake.last_name(),
+            "middleName": fake.last_name(),
+            "companyId": self.get_list_company({'active' : 'true'})[Nomber_of_Company]['id'],
+            "email": fake.unique.email(),
+            "url": fake.url(),
+            "phone": fake.phone_number(),
+            "birthdate": fake.date_of_birth(minimum_age=18, maximum_age=65).isoformat(),
+            "isActive": True
+        }
+        return requests.post(f'{self.url}employee', headers = head, json = employee_new)
+
+    ## Получить сотрудника по ID
+    def get_employee(self, id_emp):
+        response = requests.get(f'{self.url}employee/{id_emp}')
+        return response
+ 
+    ## Изменить информацию о сотруднике
+    def patch_employee(self, id_emp, isActive = True):
+        head = self.x_clients_auth()
+        employee_patch = {
+            "lastName": fake.last_name(),
+            "email": fake.unique.email(),
+            "url": fake.url(),
+            "phone": fake.phone_number(),
+            "isActive": isActive
+        }
+        response = requests.get(f'{self.url}employee/{id_emp}', headers = head, json = employee_patch)
+        return response
--- QA_SkyPro/lesson_8/test_X_Clients.py
+++ QA_SkyPro/lesson_8/test_X_Clients.py
@@ -1,23 +1,42 @@
 import requests
+from EmployeeApi import EmployeeApi
 
-base_url = "https://x-clients-be.onrender.com/"
-
-credentional = {
-    "username": "roxy",
-    "password": "animal-fairy"
-}
+api = EmployeeApi("https://x-clients-be.onrender.com/")
+   
 # **Задание. Напишите автотесты на методы приложения x-clients.** 
-
-def test_auth():
-    response = requests.post(f'{base_url}auth/login', json=credentional)
-    token = response["userToken"]
-    assert response.status_code(201)
-    return token
-
 # - [GET] /employee
+def test_employee_list_active(Nomber_of_Company = 0):
+    response = api.employee_list_active(Nomber_of_Company)
+    assert response.status_code == 200
+#     - проверяющие обязательность полей.
+
 # - [POST] /employee
+def test_add_new_employee(Nomber_of_Company = -1):
+    body = api.employee_list_active(Nomber_of_Company).json()
+    len_before = len(body)
+
+    response = api.add_new_employee(Nomber_of_Company)
+
+    body = api.employee_list_active(Nomber_of_Company).json()
+    len_after = len(body)
+    assert response.status_code == 201
+    assert len_after == len_before + 1
+#     - проверяющие обязательность полей.
+
 # - [GET] /employee/{id}
+def test_get_one_employee(Nomber_of_Company = 0):
+    result = api.add_new_employee(Nomber_of_Company).json()
+    new_id = result["id"]
+    new_employee = api.get_employee(new_id)
+    assert new_employee.status_code == 200
+    assert new_employee.json()["id"] == new_id
+#     - проверяющие обязательность полей.
+
 # - [PATCH] /employee/{id}
-# 4. Тесты должны быть двух видов:
-#     - позитивные,
+def test_employee_patch(Nomber_of_Company = 1):
+    result = api.add_new_employee(Nomber_of_Company).json()
+    new_id = result["id"]
+    patch_employee = api.patch_employee(new_id)
+    assert patch_employee.status_code == 200
+    # assert patch_employee.json()["id"] == new_id
 #     - проверяющие обязательность полей.
