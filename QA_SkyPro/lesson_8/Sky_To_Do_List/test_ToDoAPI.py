c9f182f500217253c12bc575fb3f465ca5b5c37b
Anatol-19 <anatol@staffonly39.ru>
2024-05-02 23:45:32 +0200

    lesson 8 ex 2 50%

--- QA_SkyPro/lesson_8/Sky_To_Do_List/test_ToDoAPI.py
+++ QA_SkyPro/lesson_8/Sky_To_Do_List/test_ToDoAPI.py
@@ -1,26 +1,41 @@
 import requests
+from ToDoAPI import SkyToDo
 from faker import Faker
 
-url = "https://todo-app-sky.herokuapp.com/"
+fake = Faker()
 
-class SkyToDo:
-    def __init__(self, url):
-        self.url = url
+url = "https://todo-app-sky.herokuapp.com"
+
+api = SkyToDo(url)
 
 # - Создание.
-    # def create_task(self):
-
-
+def test_create_task():
+    response, new_t = api.create_task()
+    assert response.status_code == 201
+    assert response.json()["title"] == new_t["title"]
+    
 # - Переименование.
-
+def test_rename_task():
+    resp1, new_t = api.create_task()
+    task_url = resp1.json()["url"]
+    resp2 = api.chang_title(task_url)
+    assert resp2.status_code == 200
+    assert resp2.json()["title"] != new_t
+    
 # - Удаление.
+def test_delete_task():
+    resp1 = api.create_task()[0]
+    task_url = resp1.json()["url"]
+    resp2 = api.delete_task(task_url)
+    assert resp2.status_code == 204
 
 # - Получение списка.
-    def test_get_list(self):
-        response = requests.get(url)
-        to_do_list = response.json
-        print(to_do_list)
-        return response, to_do_list
+def test_get_list():
+    response = api.get_list()
+    to_do_list = response.json
+    print(to_do_list)
+    assert response.status_code == 200
+    # assert to_do_list 
     
 # - Получение конкретной задачи из списка.
 
